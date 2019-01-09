565a6f3e2f7eec0979913205ecbd00f65c7996eb
Tim McIver <tim@timmciver.com>
2019-01-08 22:35:53 -0500

    Implement a simple UI for a CPU.

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,26 @@ import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
 
-ui :: Widget ()
-ui = withBorderStyle unicode $
-     borderWithLabel (str "Hello!") $
-     (center (str "Left") <+> vBorder <+> center (str "Right"))
+import Computer
+import CPU
+import Bus
+
+draw :: Computer -> Widget ()
+draw (Computer cpu _) = hLimit 30 $
+                        vLimit 10 $
+                        withBorderStyle unicode $
+                        borderWithLabel (str "6502 CPU") $
+                        (str $ "Reg A: " ++ (show (cpuRegA cpu))) <=>
+                        (str $ "Reg X: " ++ (show (cpuRegX cpu))) <=>
+                        (str $ "Reg Y: " ++ (show (cpuRegY cpu))) <=>
+                        (str $ "Reg P: " ++ (show (cpuRegP cpu))) <=>
+                        (str $ "stack pointer: " ++ (show (cpuStackPointer cpu))) <=>
+                        (str $ "program counter: " ++ (show (cpuProgramCounter cpu)))
+
+ui2 :: Widget ()
+ui2 = (str "Hello," <=> str "World!" <=> vBorder)
 
 main :: IO ()
-main = simpleMain ui
+main = do
+  let comp = Computer initializedCPU empty
+  simpleMain (draw comp)
