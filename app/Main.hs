01fd94dc56e3799d05de6d3bd82b44bea744decf
Tim McIver <tim@timmciver.com>
2019-01-08 20:51:25 -0500

    Set up initial configuration for using brick text UI library.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+
+ui :: Widget ()
+ui = withBorderStyle unicode $
+     borderWithLabel (str "Hello!") $
+     (center (str "Left") <+> vBorder <+> center (str "Right"))
 
 main :: IO ()
-main = someFunc
+main = simpleMain ui
